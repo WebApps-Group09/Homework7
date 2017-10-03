26610a28ae0fdda10df56b5052f75c6b6fb0c1e0
Ann Keenan <akeenan2@nd.edu>
2017-10-03 19:56:59 -0400

    query

--- set-profile.php
+++ set-profile.php
@@ -1,19 +1,23 @@
 <?php
   $db_connection = pg_connect("host=localhost dbname=homework7 user=homework7 password=homework7");
-  $columns = array("first_name", "last_name", "address", "dorm", "city", "state", "zip", "year", "quote");
+  $columns = array("first_name", "last_name", "address", "dorm", "city", "state", "year", "quote");
   foreach($columns as $column) {
     if(!empty($_POST[$column])) {
       $query = "UPDATE users SET ".$column."='".$_POST[$column]."' WHERE "."id=".$_SESSION["id"];
+      pg_query($db_connection, $query);
     }
   }
   if(!empty($_POST["zip"])) { // only non-string column
     $query = "UPDATE users SET zip=".$_POST["zip"]." WHERE "."id=".$_SESSION["id"];
+    pg_query($db_connection, $query);
   }
   if(!empty($_POST["sports"])) {
     $sports = "";
     foreach($_POST["sports"] as $sport) {
       $sports += $sport.":";
     }
+    $query = "UPDATE users SET sports=".$sports." WHERE "."id=".$_SESSION["id"];
+    pg_query($db_connection, $query);
   }
   //TODO: set db values
   header("Location: home.php");
