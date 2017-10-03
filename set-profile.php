1e554c456007a3516583daf709d65f963b607bde
Ann Keenan <akeenan2@nd.edu>
2017-10-03 19:58:24 -0400

    Add session

--- set-profile.php
+++ set-profile.php
@@ -1,17 +1,18 @@
 <?php
+  session_start();
   $db_connection = pg_connect("host=localhost dbname=homework7 user=homework7 password=homework7");
   $columns = array("first_name", "last_name", "address", "dorm", "city", "state", "year", "quote");
   foreach($columns as $column) {
     if(!empty($_POST[$column])) {
       $query = "UPDATE users SET ".$column."='".$_POST[$column]."' WHERE "."id=".$_SESSION["id"];
       //pg_query($db_connection, $query);
-      echo $query;
+      echo $query."<br>";
     }
   }
   if(!empty($_POST["zip"])) { // only non-string column
     $query = "UPDATE users SET zip=".$_POST["zip"]." WHERE "."id=".$_SESSION["id"];
     //pg_query($db_connection, $query);
-    echo $query;
+    echo $query."<br>";
   }
   if(!empty($_POST["sports"])) {
     $sports = "";
@@ -20,7 +21,7 @@
     }
     $query = "UPDATE users SET sports=".$sports." WHERE "."id=".$_SESSION["id"];
     //pg_query($db_connection, $query);
-    echo $query;
+    echo $query."<br>";
   }
   //TODO: set db values
   //header("Location: home.php");
